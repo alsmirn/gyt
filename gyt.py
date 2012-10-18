a03725db9190a311421d8202072e2f4a415521b1
Alexey Smirnov <alsmirn@gmail.com>
2012-10-18 16:31:03 +0400

    initial

---
+++ gyt.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import cmd
+from subprocess import call, check_output
+
+
+def get_git_commands():
+    """Get git commands from 'git help' output."""
+
+    names = []
+    output = check_output(["git", "help", "-a"])
+
+    for s in output.split('\n')[8:-2]:
+        names.extend(cmd.strip() for cmd in s.split())
+
+    return sorted(names)
+
+
+class Gyt(cmd.Cmd):
+    """Simple overdrive ;)"""
+
+    prompt = "git "
+
+    def do_git(self, cmd):
+        cmd = ['git', ] + cmd.split()
+        call(cmd, stdout=True)
+
+    def get_names(self):
+        return get_git_commands()
+
+    def precmd(self, line):
+        return "git %s" % line
+
+    def completenames(self, text, *ignored):
+        return [a for a in self.get_names() if a.startswith(text)]
+
+    def do_EOF(self, line):
+        return True
+
+
+if __name__ == '__main__':
+
+    try:
+        Gyt().cmdloop()
+    except KeyboardInterrupt:
+        print('^C')
+        exit(0)
